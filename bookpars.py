8521f39eee717d5cb22ae66129a66c34039569f0
Dmirti Demenko <dim.demenko@gmail.com>
2020-09-12 15:19:05 +0300

    test

--- bookpars.py
+++ bookpars.py
@@ -8,46 +8,47 @@ from multiprocessing import Pool
 list_comb=[]
 list_comb_url=[]
 res=[]
-l = ['a','e','b','f','j','t'] 
+path = "rezultat.csv"
+l = ['b','f','j','t']
 
-def combination():
+def combination(l):
     p=list(product(l, repeat=4))
     for g in p:
         strr=''
         for item in g:
             strr+=item
         list_comb.append(strr)
-def url_combination(list_comb):
     for comb in list_comb:
         stro='https://books.theoilandgasyear.com/books/'+comb
         list_comb_url.append(stro)
     
-def result(list_comb_url):
-    number=0
+def resultat(list_comb_url):
+    rt=0
     for url in list_comb_url:
         request = requests.get(url,verify= False)
         result=(request.status_code)
         if result==200:
             res.append(url)
         print(res)
-        number=number+1
-        print(number)
+        rt=rt+1
+        print(rt)
     #time.sleep(10)
 
 def csv_writer(res, path):
     with open(path, "w", newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
-        for line in data:
+        for line in res:
             writer.writerow([line])
 
+
+#def make_all(list_comb_url):
+  #  result(list_comb_url)
+   # csv_writer(resalt, path)
+
 def main():
-   
-    path = "rezultat.csv"
-    combination()
-    url_combination(list_comb)
-    result(list_comb_url)
+    combination(l)
+    resultat(list_comb_url)
     csv_writer(res, path)
 
 if __name__ == "__main__":
     main()
-    
