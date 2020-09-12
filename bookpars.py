a3a1c2ab15ee0974678e32eaa3bb62289b1bf8a1
Dmirti Demenko <dim.demenko@gmail.com>
2020-09-12 12:42:41 +0300

    test2

--- bookpars.py
+++ bookpars.py
@@ -5,11 +5,10 @@ from itertools import combinations_with_replacement as comb, product
 import time
 from multiprocessing import Pool
 
-
 list_comb=[]
 list_comb_url=[]
 res=[]
-l = ['a','e','b','f','j','t']
+l = ['a','e','b','f','j','t'] 
 
 def combination():
     p=list(product(l, repeat=4))
@@ -18,35 +17,37 @@ def combination():
         for item in g:
             strr+=item
         list_comb.append(strr)
-def url_combination():
+def url_combination(list_comb):
     for comb in list_comb:
         stro='https://books.theoilandgasyear.com/books/'+comb
         list_comb_url.append(stro)
     
-def result():
-    rt=0
+def result(list_comb_url):
+    number=0
     for url in list_comb_url:
         request = requests.get(url,verify= False)
         result=(request.status_code)
         if result==200:
             res.append(url)
         print(res)
-        rt=rt+1
-        print(rt)
+        number=number+1
+        print(number)
     #time.sleep(10)
 
-def csv_writer(data, path):
+def csv_writer(res, path):
     with open(path, "w", newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         for line in data:
             writer.writerow([line])
+
 def main():
-    data = res
+   
     path = "rezultat.csv"
     combination()
-    url_combination()
-    result()
-    csv_writer(data, path)
+    url_combination(list_comb)
+    result(list_comb_url)
+    csv_writer(res, path)
+
 if __name__ == "__main__":
     main()
     
